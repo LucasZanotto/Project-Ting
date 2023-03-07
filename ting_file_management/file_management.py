03bde8417ddb73194b881a061257230376dc77b0
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-03-07 16:54:31 -0300

    feat: implementando a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        # saida de erros sys.stderr
+        return sys.stderr.write(f"""Arquivo {path_file} não encontrado\n""")
+    if not path_file[-4:] == '.txt':
+        return sys.stderr.write("Formato inválido")
+    with open(path_file) as file:
+        file_list = file.read().split("\n")
+        print(file_list)
+        return file_list
+
+
+# txt_importer('statics/arquivo_teste.txt')
