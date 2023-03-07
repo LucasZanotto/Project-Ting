b4fd792bc490ea7a3e1d5bc2ecb13145cf39d3a9
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-03-07 18:56:31 -0300

    feat: implementando a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,27 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    files = txt_importer(path_file)
+    qtd_linhas = len(files)
+    # print(files)
+    dict_padrao = {
+           "nome_do_arquivo": path_file,
+           "qtd_linhas": qtd_linhas,
+           "linhas_do_arquivo": files
+           }
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    instance.enqueue(dict_padrao)
+    sys.stdout.write(str(dict_padrao))
+
+
+if __name__ == "__main__":
+    project = Queue()
+    process("statics/arquivo_teste.txt", project)
 
 
 def remove(instance):
