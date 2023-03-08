a1df6b779956999698d2b6bc074351ba9aabae00
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-03-08 19:06:23 -0300

    fix: arrumando tempCodeRunner

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,18 +4,18 @@ from ting_file_management.queue import Queue
 def exists_word(word, instance: Queue):
     lista = []
     for index in range(len(instance)):
-        linhas = []
+        ocorrencia = []
         frases = instance.search(index)["linhas_do_arquivo"]
         arquivo = instance.search(index)["nome_do_arquivo"]
         for linha, frase in enumerate(frases):
             if word.lower() in frase.lower():
-                linhas.append({"linha": linha + 1})
+                ocorrencia.append({"linha": linha + 1})
         result = {
                     "palavra": word,
                     "arquivo": arquivo,
-                    "ocorrencias": linhas
+                    "ocorrencias": ocorrencia
                  }
-        if bool(linhas) is False:
+        if bool(ocorrencia) is False:
             continue
         lista.append(result)
     return lista
@@ -24,18 +24,18 @@ def exists_word(word, instance: Queue):
 def search_by_word(word, instance):
     lista = []
     for index in range(len(instance)):
-        linhas = []
+        ocorrencia = []
         frases = instance.search(index)["linhas_do_arquivo"]
         arquivo = instance.search(index)["nome_do_arquivo"]
         for linha, frase in enumerate(frases):
             if word.lower() in frase.lower():
-                linhas.append({"linha": linha + 1, "conteudo": frase})
+                ocorrencia.append({"linha": linha + 1, "conteudo": frase})
         result = {
                     "palavra": word,
                     "arquivo": arquivo,
-                    "ocorrencias": linhas
+                    "ocorrencias": ocorrencia
                  }
-        if bool(linhas) is False:
+        if bool(ocorrencia) is False:
             continue
         lista.append(result)
     return lista
