00e8c9c87bdd8693234a1282147665fbb8ed8cad
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-03-08 19:00:08 -0300

    feat: implementando a função search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,10 +21,21 @@ def exists_word(word, instance: Queue):
     return lista
 
 
-if __name__ == "__main__":
-    project = Queue()
-    print(exists_word("de", project))
-
-
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    lista = []
+    for index in range(len(instance)):
+        linhas = []
+        frases = instance.search(index)["linhas_do_arquivo"]
+        arquivo = instance.search(index)["nome_do_arquivo"]
+        for linha, frase in enumerate(frases):
+            if word.lower() in frase.lower():
+                linhas.append({"linha": linha + 1, "conteudo": frase})
+        result = {
+                    "palavra": word,
+                    "arquivo": arquivo,
+                    "ocorrencias": linhas
+                 }
+        if bool(linhas) is False:
+            continue
+        lista.append(result)
+    return lista
